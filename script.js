f2799183845f77fce919529f04464ea4339cc416
Arham659 <122161432+Arham659@users.noreply.github.com>
2023-03-01 13:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
 // your code here
-let tempString ;
-let tempMsg = document.getElementById('url');
+document.querySelector("form").addEventListener("submit", (event) => {
+	event.preventDefault();
 
+	let name = document.querySelector("#name").value;
+	let year = document.querySelector("#year").value;
 
-
-function appendString() {
-    let namee = document.getElementById('name').value;
-    let year = document.getElementById('year').value;
-    tempMsg.textContent = '';
-   
- if (namee != '' && year === '') {
-        
-        tempString = `https://localhost:8080/?name=${namee}`;
-        console.log(`0`);
-        tempMsg.textContent = tempString;
-        console.log(` tempMsg.textContent`);
-    } else if (namee === '' && year != '') {
-        
-        tempString = `https://localhost:8080/?year=${year}`;
-        console.log(`1`);
-        tempMsg.textContent = tempString;
-        console.log(` tempMsg.textContent`);
-    } else if(namee != '' && year != ''){
-        console.log(`2`);
-        tempMsg.textContent = tempString;
-        console.log(` tempMsg.textContent`);
-    } else {
-        console.log(`3`);
-        tempMsg.textContent = tempString;
-        console.log(` tempMsg.textContent`);
-    } 
-}
+	document.querySelector("h3").textContent=`https://localhost:8080/?name=${name}&year=${year}`;
+});
