c0b6477c7c3cadcaf93eade96c3b420f7588044e
Arham659 <122161432+Arham659@users.noreply.github.com>
2023-03-01 13:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// your code here
-document.querySelector("form").addEventListener("submit", (event) => {
-	event.preventDefault();
-
-	let name = document.querySelector("#name").value;
-	let year = document.querySelector("#year").value;
-
-	document.querySelector("h3").textContent=`https://localhost:8080/?name=${name}&year=${year}`;
+document.getElementById("button").addEventListener("click", function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    var url;
+    if (name != "" && year != "") {
+        url = "https://localhost:8080/?" + "name=" + name + "&year=" + year;
+    } else if (name != "") {
+        url = "https://localhost:8080/?" + "name=" + name;
+    } else if (year != "") {
+        url = "https://localhost:8080/?" + "year=" + year;
+    } else {
+        url = "https://localhost:8080/";
+    }
+    document.getElementById("url").innerHTML = url;
 });
